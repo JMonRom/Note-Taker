7d208a80bb9fc0100c0af1696d0e32d54145a163
Jonathan Montes Romero <jmontes8686@gmail.com>
2021-10-11 17:20:59 -0700

    working on allowing user to post new notes on page

--- server.js
+++ server.js
@@ -9,6 +9,31 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// connected to notes html once button is clicked user redirected
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/notes.html')))
+
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')))
+
+app.get('api/notes', (req, res) => {
+  fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    if (err) throw err;
+    data  = JSON.parse(data);
+    res.json(data)
+  })
+
+  const noteContent = JSON.stringify(newNote)
+}) 
+
+app.post('/api/notes', (req, res) => {
+  const newNote = req.body;
+
+  fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    if (err) throw err; 
+  })
+
+
+});
+
 app.listen(PORT, () => {
   console.log(`App listening ${PORT}`);
 })
